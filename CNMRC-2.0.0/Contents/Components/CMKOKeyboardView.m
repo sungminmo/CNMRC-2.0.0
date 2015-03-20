93341d3a74bdb18a492e11ff75a8d4616959befa
lambert <LambertPark@gmail.com>
2015-03-20 15:03:35 +0900

    뷰 사이즈 설정.

--- CNMRC-2.0.0/Contents/Components/CMKOKeyboardView.m
+++ CNMRC-2.0.0/Contents/Components/CMKOKeyboardView.m
@@ -10,12 +10,6 @@
 
 @implementation CMKOKeyboardView
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
-}
-*/
+
 
 @end
