e958c6e48a8ddd3b39471c68fb54f230d4d460c3
lambert <LambertPark@gmail.com>
2015-03-20 14:01:50 +0900

    커스텀 키보드 클래스 추가.

---
+++ CNMRC-2.0.0/Contents/RmoteControl/CMKeyboardViewController.m
@@ -0,0 +1,37 @@
+//
+//  CMKeyboardViewController.m
+//  CNMRC-2.0.0
+//
+//  Created by Park Jong Pil on 2015. 3. 20..
+//  Copyright (c) 2015년 LambertPark. All rights reserved.
+//
+
+#import "CMKeyboardViewController.h"
+
+@interface CMKeyboardViewController ()
+
+@end
+
+@implementation CMKeyboardViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
