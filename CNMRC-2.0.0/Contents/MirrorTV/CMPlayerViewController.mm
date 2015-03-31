ecffda7df402c880b05507cf83fcdc4264ab6901
lambert <LambertPark@gmail.com>
2015-03-31 11:37:24 +0900

    신규 플레이어 추가.

---
+++ CNMRC-2.0.0/Contents/MirrorTV/CMPlayerViewController.mm
@@ -0,0 +1,37 @@
+//
+//  CMPlayerViewController.m
+//  CNMRC-2.0.0
+//
+//  Created by Park Jong Pil on 2015. 3. 31..
+//  Copyright (c) 2015년 LambertPark. All rights reserved.
+//
+
+#import "CMPlayerViewController.h"
+
+@interface CMPlayerViewController ()
+
+@end
+
+@implementation CMPlayerViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
